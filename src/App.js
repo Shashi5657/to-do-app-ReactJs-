fad5f93da753abb8a8bf73532379bbf403ae4260
Shashi5657 <sangepushashidhar@gmail.com>
2024-01-22 15:58:44 +0530

    styles need to be added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+let globalID = 0;
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function createTodo(event) {
+    event.preventDefault();
+
+    setTodos((oldTodos) => {
+      setTask("");
+      return [...oldTodos, {todo:task, id: globalID++}];
+    });
+  }
+
+  // function tryToCheckForEnterKey(e) {
+  //   if (e.keyCode === 13) {
+  //     createTodo();
+  //   }
+  // }
+
+  function deleteItem(itemID){
+    setTodos(oldTodos => oldTodos.filter(item => item.id !== itemID))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To-Do-App</h1>
+      <form>
+        <input
+          // onKeyDown={tryToCheckForEnterKey}
+          type="text"
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        />
+        <button type="submit" onClick={createTodo}>
+          Create ToDo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((item, index) => {
+          return <div  key={item.id}>
+              <li>{item.todo}</li>
+              <button onClick={()=> deleteItem(item.id)} >Delete</button>
+            </div>
+          
+        })}
+      </ul>
+    </>
   );
 }
 
